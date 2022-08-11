61d704d22018d19c3c762043232c264f43e861a2
mendesjar <ga0301@outlook.com>
2022-08-10 23:53:08 -0300

    build: create pages

---
+++ src/pages/profile/profileView.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+});
+
+export default function App() {
+    return (
+      <ImageBackground style={{ flex: 1}} source={
+        require('./assets/background.png')
+      }>
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <Text>Esse é o perfil</Text>
+        </View>
+      </ImageBackground>
+    );
+}
